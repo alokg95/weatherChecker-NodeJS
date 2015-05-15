166b6a55c93379f54c94c61ddfc9899e6078426e
Alok <alokg@me.com>
2015-05-14 23:00:10 -0700

    sdsd

--- weatherchecker.js
+++ weatherchecker.js
@@ -1,19 +1,20 @@
 var http = require("http");
 
-var request = http.get("http://api.wunderground.com/api/a0c510a81db3d0ab/conditions/q/CA/Fremont.json", function(response){
+var xxx = "fremont";
+
+var request = http.get("http://api.wunderground.com/api/a0c510a81db3d0ab/conditions/q/CA/xxx.json", function(response){
 	var body = "";
 
 	response.on('data', function(stats){
 		body += stats; 
-		
+		console.log(body);
 	});
 
 	response.on('end', function(){
 		var weather = JSON.parse(body);
-		console.log(weather);
-
+		// console.log("The current weather is " + weather.current_observation.weather + ", " + weather.current_observation.temp_f + " degrees (F).");
 	});
 
-}); 
+});
 
 
