cb35519569ad20cc0a6c9eb113dffd8662a865ec
Alok <alokg@me.com>
2015-05-14 21:56:18 -0700

    Successfully using wunderground API. JSON Parsing properly.

---
+++ index.js
@@ -0,0 +1,1 @@
+var weatherchecker = require("./weatherchecker");
--- weatherchecker.js
+++ weatherchecker.js
@@ -1,3 +1,19 @@
 var http = require("http");
 
-console.log("Hello World!");
+var request = http.get("http://api.wunderground.com/api/a0c510a81db3d0ab/conditions/q/CA/Fremont.json", function(response){
+	var body = "";
+
+	response.on('data', function(stats){
+		body += stats; 
+		
+	});
+
+	response.on('end', function(){
+		var weather = JSON.parse(body);
+		console.log(weather);
+
+	});
+
+}); 
+
+
